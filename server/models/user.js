c0f51fa2cb32b569e3d6310bc08130a53b42ee37
Tanvir <tanvirahmed1418@gmail.com>
2020-02-29 12:57:24 +0600

    78 SERVER Login users and creating tokens

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const SALT_I = 10;
+require('dotenv').config();
 
 const userSchema = mongoose.Schema({
     email:{
@@ -60,6 +62,30 @@ userSchema.pre('save', function(next){
     }
     
 })
+
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {  // cb stands for 'CALL BACK' function
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch){  // candidatePassword is given by user and this.password is from the database stored password.
+        if(err) return cb(err);
+        cb(null, isMatch)
+    })
+}
+
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;                                                  
+    var token = jwt.sign(user._id.toHexString(),process.env.SECRET)  //user '_id' from database which will further converted into hex-string and secret password from server environment(.env) 
+                                                                     //jwt = 'json web token' is required at top of user.js file.
+    user.token = token;
+    user.save(function(err,user){
+        if(err) return cb(err);
+        cb(null,user);
+    })
+}
+
+
+
+
 
 const User = mongoose.model('User', userSchema);
 
