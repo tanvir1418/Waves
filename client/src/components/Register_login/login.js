75b2a4949fa7b97a65d67660f821a02498bdec46
Tanvir <tanvirahmed1418@gmail.com>
2020-03-05 18:44:35 +0600

    94 CLIENT Finishing the login

--- client/src/components/Register_login/login.js
+++ client/src/components/Register_login/login.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import FormField from '../utils/Form/formfield';
 import { update, generateData, isFormValid } from '../utils/Form/formActions';
+import { withRouter } from 'react-router-dom';
 
 import { connect } from 'react-redux';
+import { loginUser } from '../../actions/user_actions';
 
 class Login extends Component {
 
@@ -59,7 +61,17 @@ class Login extends Component {
         let formIsValid = isFormValid(this.state.formdata, 'login');
 
         if(formIsValid) {
-            console.log(dataToSubmit)
+            this.props.dispatch(loginUser(dataToSubmit)).then(response =>{
+                if(response.payload.loginSuccess){
+                    console.log(response.payload);
+                    this.props.history.push('/user/dashboard')
+
+                } else {
+                    this.setState({
+                        formError: true
+                    })
+                }
+            });
         } else {
             this.setState({
                 formError: true
@@ -100,4 +112,4 @@ class Login extends Component {
     }
 }
 
-export default connect()(Login);
+export default connect()(withRouter(Login));
