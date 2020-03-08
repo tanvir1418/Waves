c851a5989db1d4735d0ffc5d435825fc8d35bd60
Tanvir <tanvirahmed1418@gmail.com>
2020-03-08 11:40:27 +0600

    103 CLIENT Home cards_2

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import HomeSlider from './home_slider';
 import HomePromotion from './home_promotion';
-
+import CardBlock from '../utils/card_block';
 
 import { connect } from 'react-redux';
 import { getProductsBySell, getProductsByArrival } from '../../actions/products_actions';
@@ -17,7 +17,15 @@ class Home extends Component {
         return (
             <div>
                 <HomeSlider/>
+                <CardBlock
+                    list={this.props.products.bySell}
+                    title="Best Selling Guitars"
+                />
                 <HomePromotion/>
+                <CardBlock
+                    list={this.props.products.byArrival}
+                    title="New arrivals"
+                />
             </div>
         )
     }
--- client/src/reducers/products_reducer.js
+++ client/src/reducers/products_reducer.js
@@ -1,6 +1,6 @@
 import { GET_PRODUCTS_BY_SELL, GET_PRODUCTS_BY_ARRIVAL } from '../actions/types';
 
-export default function (state={}, action){
+export default function (state={}, action) {
     switch(action.type){
         case GET_PRODUCTS_BY_SELL:
             return {...state, bySell: action.payload}
