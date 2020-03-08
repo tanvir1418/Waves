b9ac72c5b8ff66a828d367ad318f4d84be14c0f6
Tanvir <tanvirahmed1418@gmail.com>
2020-03-08 10:43:44 +0600

    102 CLIENT Home cards

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 import HomeSlider from './home_slider';
 import HomePromotion from './home_promotion';
 
+
+import { connect } from 'react-redux';
+import { getProductsBySell, getProductsByArrival } from '../../actions/products_actions';
+
 class Home extends Component {
+
+    componentDidMount(){
+        this.props.dispatch(getProductsBySell());
+        this.props.dispatch(getProductsByArrival());
+    }
+
     render() {
         return (
             <div>
@@ -12,4 +22,11 @@ class Home extends Component {
         )
     }
 }
-export default Home;
+
+const mapStateToProps = (state) => {
+    return {
+        products: state.products
+    }
+}
+
+export default connect(mapStateToProps)(Home);
