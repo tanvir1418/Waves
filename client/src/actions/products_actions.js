af0c23ebc6a410abf6d52c42b4b9fb267ca821b5
Tanvir <tanvirahmed1418@gmail.com>
2020-03-10 00:04:39 +0600

    109 SERVER Crating the shop route

--- client/src/actions/products_actions.js
+++ client/src/actions/products_actions.js
@@ -3,8 +3,10 @@ import {
     GET_PRODUCTS_BY_SELL, 
     GET_PRODUCTS_BY_ARRIVAL,
     GET_BRANDS,
-    GET_WOODS 
+    GET_WOODS,
+    GET_PRODUCTS_TO_SHOP
 } from './types';
+
 import { PRODUCT_SERVER } from '../components/utils/misc';
 
 export function getProductsBySell(){
@@ -23,6 +25,27 @@ export function getProductsByArrival(){
                     .then(response => response.data);
     return {
         type: GET_PRODUCTS_BY_ARRIVAL,
+        payload: request
+    }
+}
+
+
+export function getProductsToShop(skip, limit, filters = [], previousState = []){
+    const data = {
+        limit,
+        skip,
+        filters
+    }
+
+    const request = axios.post(`${PRODUCT_SERVER}/shop`, data)
+                    .then(response => {
+                        return {
+                            size: response.data.size,
+                            articles: response.data.articles
+                        }
+                    });
+    return {
+        type: GET_PRODUCTS_TO_SHOP,
         payload: request
     }
 }
