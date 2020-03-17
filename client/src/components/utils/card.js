6b9969cbaba99895aad748625fd643896e058d41
Tanvir <tanvirahmed1418@gmail.com>
2020-03-17 21:40:12 +0600

    128-129 CLIENT Finish the add to cart

--- client/src/components/utils/card.js
+++ client/src/components/utils/card.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import MyButton from './button';
 
+import { connect } from 'react-redux';
+import { addToCart } from '../../actions/user_actions';
+
 class Card extends Component {
 
     renderCardImage(images) {
@@ -56,7 +59,10 @@ class Card extends Component {
                             <MyButton
                                 type="bag_link"
                                 runAction = {()=>{
-                                    console.log('added to cart')
+                                    props.user.userData.isAuth ?
+                                        this.props.dispatch(addToCart(props._id))
+                                    :
+                                        console.log('You need to log in.')
                                 }}
                             />
 
@@ -69,4 +75,9 @@ class Card extends Component {
     }
 }
 
-export default Card;
+const mapStateToProps = (state) => {
+    return {
+        user: state.user
+    }
+}
+export default connect(mapStateToProps)(Card);
--- client/src/reducers/user_reducer.js
+++ client/src/reducers/user_reducer.js
@@ -1,4 +1,10 @@
-import { LOGIN_USER, REGISTER_USER, AUTH_USER, LOGOUT_USER } from '../actions/types';
+import { 
+    LOGIN_USER, 
+    REGISTER_USER, 
+    AUTH_USER, 
+    LOGOUT_USER,
+    ADD_TO_CART_USER 
+} from '../actions/types';
 
 export default function (state={}, action){
     switch(action.type){
@@ -10,6 +16,11 @@ export default function (state={}, action){
             return { ...state, userData: action.payload }
         case LOGOUT_USER:
             return { ...state }
+        case ADD_TO_CART_USER:
+            return { ...state, userData: {
+                ...state.userData,
+                cart: action.payload
+            } }
         default:
             return state;
     }
